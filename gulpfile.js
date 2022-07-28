62f302afc4fea62070971446add3b65e858e1bb2
Vadym Chutur <falloutgreck@gmail.com>
2022-07-28 21:01:27 +0300

    create gulp pack

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+let gulp = require('gulp'),
+  sass = require('gulp-sass')(require('sass')),
+  browserSync = require('browser-sync'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename');
+
+gulp.task('sass', function () {
+  return gulp
+    .src('app/scss/**/*.scss')
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('scss', function () {
+  return gulp
+    .src('app/scss/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('js', function () {
+  return gulp
+    .src([
+      'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
+  gulp.watch('app/*.html', gulp.parallel('html'));
+  gulp.watch('app/js**/*.js', gulp.parallel('script'));
+});
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: 'app/',
+    },
+  });
+});
+
+gulp.task('html', function () {
+  return gulp.src('app/**/*.html').pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('script', function () {
+  return gulp.src('app/js/**/*.js').pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task(
+  'default',
+  gulp.parallel('scss', 'sass', 'js', 'browser-sync', 'watch')
+);
